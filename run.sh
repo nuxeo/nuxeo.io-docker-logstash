c44ace26b3f8a507bb903a58183126cef47cd240
Arnaud Kervern <akervern@nuxeo.com>
2015-03-30 13:27:18 +0200

    NXIO-440: Add ES Cluster parameter

--- run.sh
+++ run.sh
@@ -2,5 +2,6 @@
 
 sed -i "s/%ESL_PORT%/${ESL_PORT}/g" /logstash/logstash/config/logstash.conf
 sed -i "s/%ESL_HOST%/${ESL_HOST}/g" /logstash/logstash/config/logstash.conf
+sed -i "s/%ESL_CLUSTER%/${ESL_CLUSTER}/g" /logstash/logstash/config/logstash.conf
 
 exec /logstash/logstash -f /logstash/logstash/config/logstash.conf
