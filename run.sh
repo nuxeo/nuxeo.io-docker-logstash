0bf56c3b4c53a09282be057788342ce7c1543b10
Arnaud Kervern <akervern@nuxeo.com>
2015-03-31 09:56:09 +0200

    NXIO-440: Use log4j input

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -
 
-sed -i "s/%ESL_PORT%/${ESL_PORT}/g" /logstash/logstash/config/logstash.conf
-sed -i "s/%ESL_HOST%/${ESL_HOST}/g" /logstash/logstash/config/logstash.conf
-sed -i "s/%ESL_CLUSTER%/${ESL_CLUSTER}/g" /logstash/logstash/config/logstash.conf
+sed -i "s/%ESL_PORT%/${ESL_PORT}/g" /logstash/config/logstash.conf
+sed -i "s/%ESL_HOST%/${ESL_HOST}/g" /logstash/config/logstash.conf
+sed -i "s/%ESL_CLUSTER%/${ESL_CLUSTER}/g" /logstash/config/logstash.conf
 
-exec /logstash/logstash -f /logstash/logstash/config/logstash.conf
+exec /logstash/bin/logstash -f /logstash/logstash/config/logstash.conf
