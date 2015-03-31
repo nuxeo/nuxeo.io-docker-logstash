3d8266472659149b952daf1068974ca1efa61629
Arnaud Kervern <akervern@nuxeo.com>
2015-03-31 15:54:25 +0200

    NXIO-440: Fix wrong path

--- run.sh
+++ run.sh
@@ -4,4 +4,4 @@ sed -i "s/%ESL_PORT%/${ESL_PORT}/g" /logstash/config/logstash.conf
 sed -i "s/%ESL_HOST%/${ESL_HOST}/g" /logstash/config/logstash.conf
 sed -i "s/%ESL_CLUSTER%/${ESL_CLUSTER}/g" /logstash/config/logstash.conf
 
-exec /logstash/bin/logstash -f /logstash/logstash/config/logstash.conf
+exec /logstash/bin/logstash -f /logstash/config/logstash.conf
